025601f2e58c2ee7b7a1094a4009158194f4d0f8
ArcaneArrow <emory@cooley.net>
2025-11-11 15:07:45 -0600

    Commiting local changes before pulling

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+from flask import Flask, request, render_template
+import sqlite3
+import os.path
 
+app = Flask(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.route_path, '.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print('Initialized the database.')
