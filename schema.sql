39b4ec58dd7b3ba9000849ac201bca5aa8905040
ArcaneArrow <emory@cooley.net>
2025-11-12 16:48:19 -0600

    Updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,15 +1,3 @@
-drop table if exists accounts;
-create table accounts (
-  id integer primary key autoincrement,
-  username text not null,
-  password text not null
-);
-drop table if exists posts;
-create table posts (
-  id integer primary key autoincrement,
-  title text not null
-);
-
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,9 +7,11 @@ CREATE TABLE users (
 );
 
 DROP TABLE IF EXISTS posts;
-CREATE TABLE posts (
-    id INTEGER PRIMARY KEY,
+CREATE TABLE posts
+(
+    id      INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
-    title TEXT NOT NULL,
-    content TEXT NOT NULL,
+    title   TEXT    NOT NULL,
+    content TEXT    NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
+);
