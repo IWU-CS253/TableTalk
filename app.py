ef72adfa2359dece4af3936d3b64c5ce4388e8a7
Demarco <dhunter@iwu.edu>
2025-11-16 22:35:47 -0600

    Changed show_recipe_card in app.py to pull from the database and connect to the html file

--- app.py
+++ app.py
@@ -117,4 +117,17 @@ def show_profile():
 
 @app.route('/recipe', methods=['post'])
 def show_recipe_card():
-    return render_template('recipe_card.html')
+    recipe_id = request.form.get("recipe_id")
+
+    db = get_db()
+
+    #This pulls the recipe itself
+    recipe = db.execute("SELECT * FROM recipes WHERE id = ?", (recipe_id)).fetchone()
+
+    #Pulls the ingredients
+    ingredients = db.execute("SELECT ingredient FROM ingredients WHERE id = ?", (recipe_id)).fetchall()
+
+    comments = db.execute("SELECT comment_text FROM comments WHERE id = ?", (recipe_id)).fetchall()
+
+    return render_template('recipe_card.html', recipe = recipe, ingredients = ingredients, comments = comments)
+    #return render_template('recipe_card.html')
